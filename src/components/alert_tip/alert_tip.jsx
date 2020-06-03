902642f732c54cebd70f50b34a28196a297aa95f
lujinggirl <lujing_girl@163.com>
2020-06-03 18:01:53 +0800

    address + name +add  + api + add_detail

--- src/components/alert_tip/alert_tip.jsx
+++ src/components/alert_tip/alert_tip.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import './alert_tip.scss'
+
 
 class AlertTip extends Component {
   static propTypes = {
     alertText: PropTypes.string.isRequired,
-    closeTip: PropTypes.func.isRequired
+    closeTip: PropTypes.func.isRequired,
+    logout: PropTypes.func
   }
   state = {
     positionY: 0,
@@ -13,6 +17,9 @@ class AlertTip extends Component {
   }
   handleClick = () => {
     this.props.closeTip()
+  }
+  handleLogout = () => {
+    this.props.logout()
   }
   render () {
     return (
@@ -23,10 +30,17 @@ class AlertTip extends Component {
             <span></span>
           </div>
           <div className='tip-text'>{this.props.alertText}</div>
-          <div className='confirm' onClick={this.handleClick}>确认</div>
+          {this.props.logout('wait')? 
+            <div className='logout' >
+              <div onClick={this.handleClick}>再等等</div>
+              <div onClick={this.handleLogout}>狠心离开</div>
+            </div>
+            :<div className='confirm' onClick={this.handleClick}>确认</div>}
+          
         </section>
       </div>
     )
   }
 }
+
 export default AlertTip
--- src/pages/set_user/address/address.jsx
+++ src/pages/set_user/address/address.jsx
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {resetUserInfo} from '@/store/user/action'
+import { is, fromJS } from 'immutable';  // 保证数据的不可变
 import {getStore} from '@/utils/commons'
-
 import './address.scss'
 import API from '../../../api/api'
 
@@ -15,36 +15,49 @@ class Address extends Component {
     userInfo: PropTypes.object
   }
   state = {
-    deletesite:false, //是否编辑状态
+    refresh:false, //是否编辑状态
   }
   getAddress = async () =>{
     const res = await API.getAddress( {},getStore('user_id'))
-    console.log(res)
     this.props.resetUserInfo('addressList', res)
+  }
+  handleDelete = (index) => {
+    let hasAddressList = this.props.userInfo.hasAddressList
+    hasAddressList.splice(index, 1)
+    this.setState({
+      refresh: !this.state.refresh
+    })
+    this.props.resetUserInfo('hasAddressList', hasAddressList)
   }
   componentWillMount () {
     this.getAddress()
   }
+
+  shouldComponentUpdate(nextProps, nextState) {   // 判断是否要更新render, return true 更新  return false不更新
+    return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
+  }
+
   render () {
     return (
       <div className='address'>
           <ul className='addresslist'>
             {
-              this.props.userInfo.addressList.map((item, index) => {
-                return  (<li>
+              this.props.userInfo.hasAddressList.map((item, index) => {
+                return  (
+                <li key={index}>
                 <div>
                   <p>{item.address}</p>
-                  <p><span>{item.phone}</span>{item.phonepk&&<span>,{item.phonepk}</span>}</p>
+                  <p><span>{item.telenum}</span>{item.standbytelenum&&<span>,{item.standbytelenum}</span>}</p>
                 </div>
-                {this.state.deletesite&&<div className='deletesite'>
-                  <span>x</span>
+                {this.props.userInfo.operate === 'delete'&&<div className='deletesite'>
+                  <span onClick={this.handleDelete.bind(this, index)}>x</span>
                 </div>}
-              </li>)
+                </li>
+              )
               })
             }
-
           </ul>
-          <Link to='/setuser/add'>
+          <Link to='/setuser/add/fromadd'>
             <div className='addsite'>
               <span>新增地址</span>
               <div className='icon-arrow-right'></div>
